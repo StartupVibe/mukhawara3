f82faeb6308404f49c686d380af68b59d6198f92
Startupvibe <238317553+StartupVibe@users.noreply.github.com>
2025-11-07 20:01:27 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ closeIcon.onclick = () => {
   nav.style.transform = "translateX(-100%)";
   document.body.style.overflow = "auto";
 };
+
+// scroll up
+let scrollIcon = document.querySelector("i.fa-solid.fa-arrow-up");
+let navSv = document.querySelector("nav");
+
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    scrollIcon.classList.add("show");
+  } else {
+    scrollIcon.classList.remove("show");
+  }
+};
+scrollIcon.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
